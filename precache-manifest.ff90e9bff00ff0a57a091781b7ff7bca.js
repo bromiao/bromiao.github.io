19af925a7a8826201e65d93b663c8e00e73572e2
bromiao <tpqmiao@gmail.com>
2019-09-24 16:47:33 +0800

    init the project

---
+++ precache-manifest.ff90e9bff00ff0a57a091781b7ff7bca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3test/robots.txt"
+  },
+  {
+    "revision": "e8b4d676a28a68377b7e7ee21efce7f3",
+    "url": "/vue3test/index.html"
+  },
+  {
+    "revision": "8557153893df6c183116",
+    "url": "/vue3test/assets/js/chunk-vendors.4e3c21b1.js"
+  },
+  {
+    "revision": "b5665179a3a4e843341e",
+    "url": "/vue3test/assets/js/app.05226656.js"
+  },
+  {
+    "revision": "29f66b47d6d9ffcd81d1",
+    "url": "/vue3test/assets/js/about.8682972e.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue3test/assets/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b5665179a3a4e843341e",
+    "url": "/vue3test/assets/css/app.96f8a6c9.css"
+  }
+];
