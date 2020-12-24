a1bc026c69af4ac66a1451770703379826f754ea
ss <ss@gmail.com>
2020-12-24 16:06:02 +0800

    correct branch

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push https://github.com/bromiao/bromiao.github.io.git master
+git push --quiet --force https://github.com/bromiao/bromiao.github.io.git master:master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
